4b328d99bdc7467f3ef4f74ae604f43aacd90d68
Mikhail Chilikov <chilikov_mihail@mail.ru>
2021-12-21 08:58:57 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
 import java.util.*;
 
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
 
     public static void main(String[] args) {
         var number = "Н555НН100";
